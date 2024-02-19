ecb3f6eddebd2f5ba6a7a6154ca15f8b68ce3e23
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2024-02-19 10:09:24 -0600

    Synchronous sample implemented

--- AzureOpenAIQuickstart/Program.cs
+++ AzureOpenAIQuickstart/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+using Azure.AI.OpenAI;
+using static System.Environment;
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+string key = GetEnvironmentVariable("AZURE_OPENAI_KEY");
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+#pragma warning disable CS8604 // Possible null reference argument.
+OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+#pragma warning restore CS8604 // Possible null reference argument.
+
+var chatCompletionsOptions = new ChatCompletionsOptions()
+{
+    DeploymentName = "gpt-35-turbo", //This must match the custom deployment name you chose for your model
+    Messages =
+    {
+        new ChatRequestSystemMessage("You are a helpful assistant."),
+        new ChatRequestUserMessage("Does Azure OpenAI support customer managed keys?"),
+        new ChatRequestAssistantMessage("Yes, customer managed keys are supported by Azure OpenAI."),
+        new ChatRequestUserMessage("Do other Azure AI services support this too?"),
+    },
+    MaxTokens = 100
+};
+
+Response<ChatCompletions> response = client.GetChatCompletions(chatCompletionsOptions);
+
+Console.WriteLine(response.Value.Choices[0].Message.Content);
+
+Console.WriteLine();
